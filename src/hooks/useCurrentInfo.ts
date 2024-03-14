1a93a07c1e708762ef82f73d2a323e757960c77f
Phowebing <phowebing@gmail.com>
2024-03-14 11:37:54 +0900

    docs : tailwind 적용

--- src/hooks/useCurrentInfo.ts
+++ src/hooks/useCurrentInfo.ts
@@ -1,17 +1,21 @@
-import { useCallback } from "react";
+import { Info } from '@/types/info';
+import { useCallback } from 'react';
+import { mutate } from 'swr';
 // SWR 보관용 구분키
-export const CURRENT_INFO_KEY='/health-info'
+export const CURRENT_INFO_KEY = '/health-info';
 
 export const useCurrentInfo = () => {
-    // 현재 좌표정보를 SWR에 저장
-const setCurrentInfo=useCallback(()=>{
+  // 현재 좌표정보를 SWR 에 저장하기
+  const setCurrentInfo = useCallback((info: Info) => {
     // SWR 업데이트
-},[])
+    mutate(CURRENT_INFO_KEY, info);
+  }, []);
 
-// 저장된 좌표를 SWR에 지우기
-const clearCurrentInfo=useCallback(()=>{
+  // 저장된 좌표정를 SWR 에 지우기
+  const clearCurrentInfo = useCallback(() => {
     // SWR 초기화
-},[])
-    return {setCurrentInfo,clearCurrentInfo};
+    mutate(CURRENT_INFO_KEY, null);
+  }, []);
 
+  return { setCurrentInfo, clearCurrentInfo };
 };
--- src/types/info.ts
+++ src/types/info.ts
@@ -4,9 +4,9 @@ type Lat = number;
 type Lng = number;
 // 위도와 경도를 묶어준 타입
 export type Coordinates = [Lat, Lng];
-// json 에서 온 데이터를 위한 타입 정의
+// json 에서 온 데이터를 위한 타입정의
 export type Info = {
   coordinates: Coordinates;
-  //   건강증진센터구분?:string;
-  //   운영시작시각?:string;
+  //   건강증진센터구분?: string;
+  //   운영시작시각?: string;
 };
